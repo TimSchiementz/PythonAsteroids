b7288bc65ca4d535457f7e17be9b115ce0105c90
TimSchiementz <tim.schiementz@gmail.com>
2025-10-13 14:45:33 +0200

    feat: manage FPS

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
