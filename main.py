2911ad725470b2d36bf591518d8e14faf7925e4e
TimSchiementz <tim.schiementz@gmail.com>
2025-10-13 15:23:18 +0200

    feat: add player with movement and add asteroid objects

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 import pygame
+
+from asteroidfield import AsteroidField
 from constants import *
+from player import Player
+from asteroid import Asteroid
+
 def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Set up the clock for a framerate
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = updatable
+    # Spawn Player in the middle of the screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
     while True:
         screen.fill("black")
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
+
+
+        updatable.update(dt)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
